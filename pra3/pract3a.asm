021d7d843d55e02d0a760b77582f66e115f615b3
PabloRS1420 <36885631+PabloRS1420@users.noreply.github.com>
2019-04-01 16:33:06 +0200

    Update pract3a.asm

--- pra3/pract3a.asm
+++ pra3/pract3a.asm
@@ -50,70 +50,67 @@ _computeControlDigit ENDP
 
 _decodeBarCode PROC FAR	
 ;decodeBarCode(char (DB) (cadena de 13) barCodeStr, int (DW) (3) &countryCode, int (DW) (4) &companyCode, long (5) &productCode, char (1) &controlDigit)
-; AL, BX, CX, AX:DX 
-	;SACAR LA CADENA barCodeStr
-	MOV BX, SP+6
+;decodeBarCode(char(cadena de 13) barCodeStr, int(3) &countryCode, long(4) &companyCode, long(5) &productCode, char(1) &controlDigit)
+	PUSH BP;
+	MOV BP, SP;
+;barCodeStr [845 6789 01234 5]
+	MOV DI, [BP+6]
 	
-ITERSTR1:		; CONVERTIR UN CARACTER
-	MOV DX, 0
-	MOV DL, [BX] 
-	SUB DL, 30H 	; CONVERCION: STRING -> INT
-	PUSH DX		; METE CARACTER CONVERTIDO A PILA
-	INC BX
-	CMP BL, MAT1[1]
-	JNE ITERSTR1
+;countryCode [845]
+	LES BX, [BP+10]
+	MOV SI, ES
 	
-	; METE EL NUMERO DESDE LA PILA A MATRIZ
-	MOV NUMERO, 0
-	MOV CX, 1	
-ITERINT1:
-	POP DX
+	;MOV DH, [DI]
+	;MOV DL, [DI+2]
+	MOV DX, 0000H
+	
+	;MOV [BX], DH
+	;MOV [BX+2], DL
+	
+	;MOV [BX], DX
+	MOV [SI], DX
+	
+;companyCode [6789]
+	LES BX, [BP+14]
+	MOV SI, ES
+	
+	;MOV DH, [DI+4]
+	;MOV DL, [DI+6]
+	MOV DX, 0000H
+	
+	;MOV [BX+1], DH
+	;MOV [BX+3], DL 
+	
+	;MOV [BX], DX
+	MOV [SI], DX
+	
+;productCode [01234]
+	LES BX, [BP+18]
+	MOV SI, ES
+	
+	;MOV CL, [DI+7]
+	;MOV DH, [DI+9]
+	;MOV DL, [DI+11]
+	MOV DX, 0000H
+	
+	;MOV [BX], CL
+	;MOV [BX+2], DH
+	;MOV [BX+4], DL 
+	
+	;MOV [BX], DX
+	MOV [SI], DX
+	
+;controlDigit [5]
+	LES BX, [BP+22]
+	MOV SI, ES
+	
+	;MOV DH, [DI+12]
+	MOV DH, 00
+	
+	;MOV [BX], DH
+	MOV [SI], DH
 
-	MOV AX, CX
-	MUL DX		; DX * 10^N
-	MOV DX, AX
-	PUSH DX
-	MOV AX, 10
-	MUL CX	 	; CX * 10
-	MOV CX, AX 	
-	POP DX	
-	ADD NUMERO, DX
-
-	DEC BL
-	CMP BL, 0
-	JNE ITERINT1
-
-	; PREPARA A QUE SE MUEVE DX A MATRIZ
-	MOV DX, NUMERO
-	MOV MATRIZ[0], DL
-
-	
-	MOV , [BX+1] 
-	MOV , [BX+2] 
-	MOV SP+8, ;countryCode [0123]
-	
-	MOV , [BX+3] 
-	MOV , [BX+4] 
-	MOV , [BX+5] 
-	MOV , [BX+6] 
-	
-	MOV SP+10, ;companyCode [1234]
-	
-	MOV , [BX+7] 
-	
-	MOV SP+12, ;productCode [0001]
-	
-	MOV , [BX+8] 
-	MOV , [BX+9] 
-	MOV , [BX+10] 
-	MOV , [BX+11] 
-	
-	MOV SP+14, ;productCode [2345]
-	
-	MOV BL, [BX+12] 
-	MOV BH, 00H
-	
-	MOV SP+16, BX ;controlDigit [0001]
+	POP BP
 	RET
 _decodeBarCode ENDP
 _CODE ENDS 
